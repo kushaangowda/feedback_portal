c5bb33efa96bba5246518074508e2d6870bc038f
Kushaan Gowda <kushaangowda@yahoo.com>
2023-07-03 00:01:56 +0530

    final changes

--- src/routes/Root.js
+++ src/routes/Root.js
@@ -1,6 +1,13 @@
-import { Link, Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 
 export default function Root() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/home");
+  }, [navigate]);
+
   return (
     <>
       <div id="sidebar">
